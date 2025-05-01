91c9e3d535ebc8db6a485c1973ccd2c06ad726a4
potlitel@gmail.com <ajorge@fsa-ltda.com>
2025-05-01 17:48:06 -0400

    Add GetServiceOrdersDocumentsBySOIdHandler handler.

---
+++ WebApiSO/Controllers/ServiceOrdersDocuments/GetAllServiceOrdersDocumentsBySOId.cs
@@ -0,0 +1,20 @@
+﻿using FSA.Core.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using WebApiSO.Data.Dtos;
+using WebApiSO.Features.ServiceOrderDocuments.GetAllBySOId;
+
+namespace WebApiSO.Controllers.ServiceOrdersDocuments
+{
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "Service Orders: Documents")]
+    [Route("api/so/documents/all")]
+    public class GetAllServiceOrdersDocumentsBySOId(GetServiceOrdersDocumentsBySOIdHandler handler) : ControllerBase
+    {
+
+        [HttpGet("{id_so:int}")]
+        public async Task<Result<IEnumerable<ServiceOrderDocDto>>> Get(long id_so)
+        {
+            return await handler.Handle(id_so);
+        }
+    }
+}
--- WebApiSO/Features/ServiceOrderDocuments/GetAllBySOId/GetServiceOrdersDocumentsBySOIdHandler.cs
+++ WebApiSO/Features/ServiceOrderDocuments/GetAllBySOId/GetServiceOrdersDocumentsBySOIdHandler.cs
@@ -3,26 +3,23 @@ using FSA.Core.Dtos;
 using FSA.Core.Extensions;
 using FSA.Core.Interfaces;
 using FSA.Core.ServiceOrders.Models;
+using FSA.Core.ServiceOrders.Models.Masters;
 using FSA.Core.Utils;
 using System.Linq.Dynamic.Core;
 using WebApiSO.Data.Dtos;
-using ServiceOrderRegisterDto = WebApiSO.Data.Dtos.ServiceOrderRegisterDto;
 
-namespace WebApiSO.Features.ServiceOrderRegisters
+namespace WebApiSO.Features.ServiceOrderDocuments.GetAllBySOId
 {
-    public class GetServiceOrdersRegistersBySOIdHandler : IServiceHandler<long, IEnumerable<ServiceOrderRegisterDto>>
+    public class GetServiceOrdersDocumentsBySOIdHandler : IServiceHandler<long, IEnumerable<ServiceOrderDocDto>>
     {
         private readonly IRepository repository;
 
-        private readonly AppDbContext appDbContext;
-
-        public GetServiceOrdersRegistersBySOIdHandler([FromKeyedServices("SO_Repository")] IRepository repository, AppDbContext appDbContext)
+        public GetServiceOrdersDocumentsBySOIdHandler([FromKeyedServices("SO_Repository")] IRepository repository)
         {
             this.repository = repository;
-            this.appDbContext = appDbContext;
         }
 
-        public async Task<Result<IEnumerable<ServiceOrderRegisterDto>>> Handle(long id)
+        public async Task<Result<IEnumerable<ServiceOrderDocDto>>> Handle(long id)
         {
             Pagination pagination = new Pagination(10);
 
@@ -31,17 +28,17 @@ namespace WebApiSO.Features.ServiceOrderRegisters
                 errors.Add($"{typeof(ServiceOrder).Name} not found");
 
             if (errors.Count > 0)
-                return Result<IEnumerable<ServiceOrderRegisterDto>>.Failure(errors, CustomStatusCode.StatusBadRequest);
+                return Result<IEnumerable<ServiceOrderDocDto>>.Failure(errors, CustomStatusCode.StatusBadRequest);
 
-            var entityList = repository.Entity<ServiceOrderRegister>().Where(t => t.ServiceOrderId == id);
+            var entityList = repository.Entity<ServiceOrderDocument>().Where(t => t.ServiceOrderId == id);
 
             entityList = Search(entityList, pagination);
 
-            var result = entityList.ApplyPagination(pagination).ToDynamicList<ServiceOrderRegister>().Select(ServiceOrderRegisterDto.ToDto).ToList();
-            
+            var result = entityList.ApplyPagination(pagination).ToDynamicList<ServiceOrderDocument>().Select(ServiceOrderDocDto.ToDto).ToList();
+
             await AttachRelatedObject(id, result);
 
-            return Result<IEnumerable<ServiceOrderRegisterDto>>.SuccessWith(result!, pagination, CustomStatusCode.StatusOk);
+            return Result<IEnumerable<ServiceOrderDocDto>>.SuccessWith(result!, pagination, CustomStatusCode.StatusOk);
         }
 
         /// <summary>
@@ -50,21 +47,23 @@ namespace WebApiSO.Features.ServiceOrderRegisters
         /// <param name="id"></param>
         /// <param name="result"></param>
         /// <returns></returns>
-        private async Task AttachRelatedObject(long id, List<ServiceOrderRegisterDto> result)
+        private async Task AttachRelatedObject(long id, List<ServiceOrderDocDto> result)
         {
             //Get ServiceOrder objecto to attach to response
             var entityExtra = await repository.GetByIdAsync<ServiceOrder>(id);
+            var documentTypes = repository.Entity<DocumentType>();
 
             foreach (var item in result)
             {
                 item.ServiceOrder = CustomServiceOrderDto.ToDto(entityExtra);
+                item.DocumentType = DocumentTypeDto.ToDto(await documentTypes.FirstOrDefaultAsync(t => t.Id == item.DocumentTypeId));
             }
         }
 
-        private IQueryable<ServiceOrderRegister> Search(IQueryable<ServiceOrderRegister> query, Pagination pagination)
+        private IQueryable<ServiceOrderDocument> Search(IQueryable<ServiceOrderDocument> query, Pagination pagination)
         {
             if (!string.IsNullOrEmpty(pagination.FilterTerm))
-                return query.Where(q => q.Observations!.Contains(pagination.FilterTerm));
+                return query.Where(q => q.Name!.Contains(pagination.FilterTerm));
             return query;
         }
     }
